37d58b26793c1cf3d59e3ae02966f68416ab464f
Baekshook <ecgo0106@naver.com>
2023-08-01 17:46:48 +0900

    change key NEXT_PUBLIC

--- frontend/pages/api/auth/[...nextauth].jsx
+++ frontend/pages/api/auth/[...nextauth].jsx
@@ -6,16 +6,16 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET,
     }),
     NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID,
-      clientSecret: process.env.NAVER_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
   ],
   secret: "potatoSecret",
