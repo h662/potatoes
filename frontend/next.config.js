d46b74f188906f7993f033fcee05671e5d243a87
seon-2 <phia130@naver.com>
2023-08-02 07:01:50 +0900

    next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      encoding: false,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+
+    return config;
   },
 };
 
